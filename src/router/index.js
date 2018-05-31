10a3e46966c29b56269ffc18274649157c1e0c01
Hale Deng <haledeng88@gmail.com>
2018-05-31 23:16:27 +0800

    add MovieTable router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'views/home'
 import MovieDetail from 'views/movie-detail'
+import MovieTable from 'views/movie-table'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/movie/:id',
       name: 'MovieDetail',
       component: MovieDetail
+    },
+    {
+      path: '/admin/movie/list',
+      name: 'MovieTable',
+      component: MovieTable
     }
   ]
 })
